e42fb5bcc037c5d9c3d1df5d11168cf3ab40fe5d
Vladyslav <53900308+ShinoaLightner@users.noreply.github.com>
2023-04-22 16:43:11 +0300

    share=True

--- webui.py
+++ webui.py
@@ -255,7 +255,7 @@ def webui():
                     gradio_auth_creds += [x.strip() for x in line.split(',') if x.strip()]
 
         app, local_url, share_url = shared.demo.launch(
-            share=cmd_opts.share,
+            share=True,
             server_name=server_name,
             server_port=cmd_opts.port,
             ssl_keyfile=cmd_opts.tls_keyfile,
